9041b66ccd3866212e9c8d1c3a768cd4527dcebd
HP <jenishumaretiya56@gmail.com>
2023-06-04 10:27:20 +0530

    jenish

---
+++ lib/demo.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class demo extends StatefulWidget {
+  const demo({Key? key}) : super(key: key);
+
+  @override
+  State<demo> createState() => _demoState();
+}
+
+class _demoState extends State<demo> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
